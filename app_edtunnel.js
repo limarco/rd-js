a7c23f914c798bd7295fa0d53c29064bcb3c6d2b
limarco <117861332+limarco@users.noreply.github.com>
2023-07-28 23:41:55 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '4269b92e-c9e9-4cb6-8102-c54f5afac23f';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
